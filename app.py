4dc3ab80938c7eb71b6d3e051c93d1a3dae74fa3
Tsheung <tobylsheung@gmail.com>
2022-10-09 15:28:22 -0400

    Add template base.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
 def index():
-    return "Hello World"
+    return render_template("base.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
