db6754cafd5787ca60a67963e3d94c1ad2e5d45d
Tsheung <tobylsheung@gmail.com>
2022-10-09 21:49:33 -0400

    Add update and delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,9 +13,31 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    list_to = Todo.query.all()
-    print(list_to)
-    return render_template("base.html", list_to=list_to)
+    list_todo = Todo.query.all()
+    print(list_todo)
+    return render_template("base.html", list_todo=list_todo)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     with app.app_context():
